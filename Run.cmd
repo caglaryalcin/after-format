f211aa2643781c3cbc5242a9dcbf36f6e73f5858
Çağlar Yalçın <iamcaglar@pm.me>
2022-01-12 18:20:39 +0300

    Update Run.cmd

--- Run.cmd
+++ Run.cmd
@@ -1,1 +1,1 @@
-@powershell.exe -NoProfile -ExecutionPolicy Bypass -File "C:\after format\Admin.ps1" -include "C:\after format\Shells.psm1" -preset "C:\after format\Functions.preset"
+@powershell.exe -NoProfile -ExecutionPolicy Bypass -File "C:\after-format-main\Admin.ps1" -include "C:\after-format-main\Shells.psm1" -preset "C:\after-format-main\Functions.preset"
